8bf6356cfd11a00f2c381a6246e1fa00db3d4f92
unknown <harshborsadiya@gmail.com>
2024-03-08 11:31:24 +0530

    Change The Pattern Of Shopo name

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -7,10 +7,11 @@ import { HiOutlineShoppingBag } from "react-icons/hi";
 
 const HeaderMain = () => {
   return (
+    <>
     <div className="border-b border-gray-200 py-6">
       <div className="container sm:flex justify-between items-center">
         <div className="font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish">
-          H's STORE
+        H&apos;s STORE
         </div>
 
         <div className="w-full sm:w-[300px] md:w-[70%] relative">
@@ -45,6 +46,7 @@ const HeaderMain = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
