1f2e51a01df0539d7b9706171b16d7092cb4359a
unknown <harshborsadiya@gmail.com>
2024-03-08 12:42:20 +0530

    Added A 2 Pics Of Shirt On Menis Product Page

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -12,9 +12,9 @@ const AboutUs: React.FC = () => {
         experience.
       </p>
       <div className="flex justify-center items-center gap-6">
-        <Image className="w-1/4 shadow-lg hover:shadow-2xl" src="https://cdn.pixabay.com/photo/2016/11/22/19/08/hangers-1850082_1280.jpg" alt="" />
-        <Image className="w-1/4 shadow-lg hover:shadow-2xl" src="https://cdn.pixabay.com/photo/2019/04/08/15/07/gold-4112176_1280.jpg" alt="" />
-        <Image className="w-1/4 shadow-lg hover:shadow-2xl" src="https://cdn.pixabay.com/photo/2016/06/15/22/22/still-life-1460067_1280.jpg" alt="" />
+        <Image className="w-1/4 shadow-lg hover:shadow-2xl" src="a1.jpg" alt="" />
+        <Image className="w-1/4 shadow-lg hover:shadow-2xl" src="a2.jpg" alt="" />
+        <Image className="w-1/4 shadow-lg hover:shadow-2xl" src="a3.jpg" alt=""  />
       </div>
       <Link href="Contact">
       <button className="border-green-700 border-2 p-2 rounded-md">
